4546fed7f1b80cb4d024aeae8630c5facd4c7b21
HuseyinUzun <118770011+HuseyinUzun@users.noreply.github.com>
2023-03-07 15:03:36 +0300

    Add files via upload

---
+++ KarakterleriTerstenYazdırmaAppC/KarakterleriTerstenYazdırmaAppC/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarakterleriTerstenYazdırmaAppC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cumle, terscumle = "";
+            Console.Write("Kelime giriniz : ");
+            cumle = Console.ReadLine();
+            for (int i =cumle.Length-1; i>=0; i--)
+            {
+                terscumle += cumle[i];
+            }
+            Console.WriteLine(terscumle);
+            Console.ReadKey();
+        }
+    }
+}
